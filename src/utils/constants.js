b277b5d4c4fded14b6e1f020abdb5366fa45437e
jeanmo333 <65782134+jeanmo333@users.noreply.github.com>
2023-03-10 17:38:01 -0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,8 @@
 /** @format */
 
 export const API_URL =
-  "https://api-node-ecommerce-reactnative-production.up.railway.app";
 // export const API_URL = "http://192.168.1.3:4000";
 export const TOKEN = "token";
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
 export const STRAPI_PUBLISHABLE_KEY =
-  "pk_test_51KtO9tLkG7uP1HwYqUixy0mafGx9cf89RLH936C2l3LUhL0nKqAHjEy8FIXFkpS70c7tBhfByKHtF2sc0AJQjcBr00gm07ckaz";
